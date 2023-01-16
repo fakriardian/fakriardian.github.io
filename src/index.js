71b80b52f8eebd5dc42b95e9feb93672d47d7587
Irfan Fakri Ardian <mazbro9988@gmail.com>
2023-01-16 11:51:42 +0700

    change react router

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <HashRouter >
+      <App />
+    </HashRouter >
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
